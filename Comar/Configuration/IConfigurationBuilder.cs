e19edd46f51cf4acb96da2f2a07c2a245a2caed6
Camille Galladjov <ujinjinjin@outlook.com>
2022-03-01 00:51:23 +0300

    create configuration infrastructure

---
+++ Comar/Configuration/IConfigurationBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace Comar.Configuration;
+
+/// <summary>  </summary>
+public interface IConfigurationBuilder
+{
+	/// <summary>  </summary>
+	/// <param name="provider"></param>
+	/// <returns></returns>
+	IConfigurationBuilder AddProvider(IConfigurationProvider provider);
+
+	/// <summary>  </summary>
+	/// <returns></returns>
+	IConfiguration Build();
+}
