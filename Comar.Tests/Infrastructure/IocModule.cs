3e113c69e914c679b389a1192c217aaeab7429d5
Camille Galladjov <ujinjinjin@outlook.com>
2022-03-28 22:34:05 +0300

    in memory configuration provider + tests

--- Comar.Tests/Infrastructure/IocModule.cs
+++ Comar.Tests/Infrastructure/IocModule.cs
@@ -1,6 +1,8 @@
 ﻿using Comar.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Comar.Tests.Infrastructure;
 
@@ -14,8 +16,21 @@ public class IocModule : IIocModule
 
 		collection.AddConfiguration()
 			.AddFileProvider()
-			.AddInMemoryProvider();
+			.AddInMemoryProvider(GetDefaultConfiguration());
 
 		return collection.BuildServiceProvider();
+	}
+
+	private IDictionary<string, string?> GetDefaultConfiguration()
+	{
+		var configuration = new ConcurrentDictionary<string, string?>();
+
+		configuration.TryAdd("property-1", "value-1");
+		configuration.TryAdd("property-2", "value-2");
+		configuration.TryAdd("property-3", "value-3");
+		configuration.TryAdd("property-4", "value-4");
+		configuration.TryAdd("property-5", "value-5");
+
+		return configuration;
 	}
 }
