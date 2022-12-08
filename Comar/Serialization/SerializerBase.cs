d2962e088dfdf7c85cf5d37b30b2c690e223010c
Camille Galladjov <ujinjinjin@outlook.com>
2022-12-08 22:34:52 +0400

    CRLF > LF & spaces > tabs

--- Comar/Serialization/SerializerBase.cs
+++ Comar/Serialization/SerializerBase.cs
@@ -5,61 +5,61 @@ namespace Comar.Serialization;
 
 internal abstract class SerializerBase : ISerializer
 {
-    /// <inheritdoc />
-    public T Deserialize<T>(string contents)
-    {
-        try
-        {
-            return TryDeserialize<T>(contents);
-        }
-        catch (Exception e)
-        {
-            throw new SerializationException(default, e);
-        }
-    }
-    
-    private T TryDeserialize<T>(string contents)
-    {
-        if (string.IsNullOrWhiteSpace(contents))
-        {
-            throw new SerializationException();
-        }
+	/// <inheritdoc />
+	public T Deserialize<T>(string contents)
+	{
+		try
+		{
+			return TryDeserialize<T>(contents);
+		}
+		catch (Exception e)
+		{
+			throw new SerializationException(default, e);
+		}
+	}
 
-        var obj = DeserializeObject<T>(contents);
-        if (obj is null)
-        {
-            throw new SerializationException();
-        }
+	private T TryDeserialize<T>(string contents)
+	{
+		if (string.IsNullOrWhiteSpace(contents))
+		{
+			throw new SerializationException();
+		}
 
-        return obj;
-    }
+		var obj = DeserializeObject<T>(contents);
+		if (obj is null)
+		{
+			throw new SerializationException();
+		}
 
-    protected abstract T? DeserializeObject<T>(string contents);
+		return obj;
+	}
 
-    /// <inheritdoc />
-    public string Serialize<T>(T data)
-    {
-        try
-        {
-            return TrySerialize(data);
-        }
-        catch (Exception e)
-        {
-            throw new SerializationException(default, e);
-        }
-    }
+	protected abstract T? DeserializeObject<T>(string contents);
 
-    private string TrySerialize<T>(T data)
-    {
-        if (data is null)
-        {
-            throw new SerializationException();
-        }
+	/// <inheritdoc />
+	public string Serialize<T>(T data)
+	{
+		try
+		{
+			return TrySerialize(data);
+		}
+		catch (Exception e)
+		{
+			throw new SerializationException(default, e);
+		}
+	}
 
-        var s = SerializeObject(data);
+	private string TrySerialize<T>(T data)
+	{
+		if (data is null)
+		{
+			throw new SerializationException();
+		}
 
-        return s.ToUnixEol();
-    }
-    
-    protected abstract string SerializeObject<T>(T data);
+		var s = SerializeObject(data);
+
+		return s.ToUnixEol();
+	}
+
+	protected abstract string SerializeObject<T>(T data);
 }
